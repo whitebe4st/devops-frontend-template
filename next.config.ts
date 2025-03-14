a5143602d4818b1ef3aa5d5a5963263b473e375f
whitebeast <natthapat.y@ku.th>
2025-03-14 20:34:29 +0700

    edit frontent

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   /* config options here */
+  output: 'standalone',
 };
 
 export default nextConfig;
